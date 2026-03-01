0a0e344fa46bc642f9f6fdb2b16049cafe4893a0
burgossrodrigo <rodrigoburgos@live.com>
2026-03-01 21:51:10 +0000

    WIP: bridge send e receive

--- contracts/solana/programs/bridge/src/instructions/mod.rs
+++ contracts/solana/programs/bridge/src/instructions/mod.rs
@@ -1,7 +1,9 @@
 pub mod add_token;
+pub mod bridge_send;
 pub mod initialize;
 pub mod remove_token;
 
 pub use add_token::*;
+pub use bridge_send::*;
 pub use initialize::*;
 pub use remove_token::*;
