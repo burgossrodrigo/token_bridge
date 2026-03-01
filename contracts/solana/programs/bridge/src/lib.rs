db6719fe1fd46c1d41010bd503d0ad4c885e873f
burgossrodrigo <rodrigoburgos@live.com>
2026-03-01 18:13:31 +0000

    WIP: solana setup

---
+++ contracts/solana/programs/bridge/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5vWinfDfVpV4Q6G8a9fmu9HnLQ4GwK3oP5P6ZTLG2qLg");
+
+#[program]
+pub mod solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
