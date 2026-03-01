d8613a909ded243caced122b36a9aa2603a46be7
burgossrodrigo <rodrigoburgos@live.com>
2026-03-01 18:43:01 +0000

    feat: events module

---
+++ contracts/solana/programs/bridge/src/events.rs
@@ -0,0 +1,25 @@
+use anchor_lang::prelude::*;
+
+#[event]
+pub struct TokenSent {
+    pub to: Pubkey,
+    pub mint: Pubkey,
+    pub amount: u64,
+}
+
+#[event]
+pub struct TokenReceived {
+    pub from: Pubkey,
+    pub mint: Pubkey,
+    pub amount: u64,
+}
+
+#[event]
+pub struct AdminSet {
+    pub admin: Pubkey,
+}
+
+#[event]
+pub struct AdminRemoved {
+    pub admin: Pubkey,
+}
